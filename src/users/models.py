6ed49105586d66b6166f24cbd61213211e8a0cc1
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-04-27 16:06:04 +0200

    Modeles User et Project

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,24 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **other_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **other_fields):
+        user = self.create_user(email, password=password, **other_fields)
+        user.is_admin = True
+        user.is_staff = True
+        user.save()
+        return user
 
 
 class User(AbstractBaseUser):
@@ -7,7 +26,19 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
+    objects = UserManager()
+
     first_name = models.CharField(max_length=128, blank=True)
     last_name = models.CharField(max_length=128, blank=True)
     email = models.CharField(max_length=128, blank=False, unique=True)
     password = models.CharField(max_length=128, blank=False)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
